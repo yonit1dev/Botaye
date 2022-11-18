af434de808aa7f5189f12389e96ebb297c155126
yonit1dev <yonit.anosie@gmail.com>
2022-11-18 10:09:37 +0300

    React build - separated vanillajs from new build. - started reactjs build.

---
+++ vanilla/src/components/sharedPlace.js
@@ -0,0 +1,18 @@
+import { DOM, MAP } from "../utils/globals";
+
+class SharedPlace {
+  constructor(coords, address) {
+    this.url = 
+    this.coords = {
+      lat,
+      lng,
+    };
+    this.address;
+  }
+
+  collectParams() {
+
+  }
+
+  renderSharedPlace() {}
+}
