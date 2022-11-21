c48faf7fb00b7f42da002b0573c1dfb78cd03ea7
yonit1dev <yonit.anosie@gmail.com>
2022-11-21 15:24:46 +0300

    code refactor

--- react/src/app.js
+++
@@ -1,5 +0,0 @@
-function App() {
-    return 
-}
-
-export { App };
--- react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./app";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<h1>Hello, World</h1>);
