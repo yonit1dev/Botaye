219851d5474e31bf5ef2328bacdd3678cec070c0
yonit1dev <yonit.anosie@gmail.com>
2023-02-27 16:55:07 +0300

    boilerplate modi

--- app-server/src/main.ts
+++ app-server/src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./config/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 3000;
+
+  await app.listen(PORT).then(async () => {
+    const url = await app.getUrl();
+    console.log("Listening on: ", url);
+  });
 }
+
 bootstrap();
